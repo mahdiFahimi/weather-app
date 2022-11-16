cb56a92d9db40d6089e71d493d392fbe921fe6ec
Mahdi Fahimi <ma.79.fahimi@gmail.com>
2022-11-16 18:23:19 +0330

    working on it

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import './App.css';
+import axios from 'axios';
+import {useEffect} from 'react'
 
 function App() {
+  const url = 'https://course-api.com/react-store-products';
+
+    const fetchData = async () => {
+      try{
+        const response = await axios(url);
+        console.log(response)
+
+      } catch(e){
+        console.log(e.response)
+      }
+    }
+
+
+  useEffect(() =>{
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
       
